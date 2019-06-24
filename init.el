e6cf9a5b7253af723d21ee9b3490f1ff5e9afa81
Wang, Gang <qichfan@gmail.com>
2019-06-23 21:07:05 -0700

    Create init.el

---
+++ init.el
@@ -0,0 +1,504 @@
+;; selectively copied from https://github.com/howardabrams/dot-files/blob/master/emacs.org
+
+(setq gc-cons-threshold 50000000)
+
+(setq gnutls-min-prime-bits 4096)
+
+(require 'package)
+
+(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
+                         ("gnu"       . "http://elpa.gnu.org/packages/")
+                         ("melpa"     . "http://melpa.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+
+(package-initialize)
+(package-refresh-contents)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+(require 'cl)
+
+(use-package dash
+  :ensure t
+  :config (eval-after-load "dash" '(dash-enable-font-lock)))
+
+(use-package s
+  :ensure t)
+
+(use-package f
+  :ensure t)
+
+(setq-default indent-tabs-mode nil)
+(setq tab-width 2)
+
+(setq-default tab-always-indent 'complete)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(setq scroll-conservatively 10000
+      scroll-preserve-screen-position t)
+
+(setq disabled-command-function nil)
+
+(setq initial-scratch-message "") ;; Uh, I know what Scratch is for
+(setq visible-bell t)             ;; Get rid of the beeps
+
+(when (window-system)
+  (tool-bar-mode 0)               ;; Toolbars were only cool with XEmacs
+  (when (fboundp 'horizontal-scroll-bar-mode)
+    (horizontal-scroll-bar-mode -1))
+  (scroll-bar-mode -1))            ;; Scrollbars are waste screen estate
+
+(set-face-background 'region "blue3")
+
+(use-package whitespace
+  :bind ("C-c T w" . whitespace-mode)
+  :init
+  (setq whitespace-line-column nil
+        whitespace-display-mappings '((space-mark 32 [183] [46])
+                                      (newline-mark 10 [9166 10])
+                                      (tab-mark 9 [9654 9] [92 9])))
+  :config
+  (set-face-attribute 'whitespace-space       nil :foreground "#666666" :background nil)
+  (set-face-attribute 'whitespace-newline     nil :foreground "#666666" :background nil)
+  (set-face-attribute 'whitespace-indentation nil :foreground "#666666" :background nil)
+  :diminish whitespace-mode)
+
+(use-package fill
+  :bind (("C-c T f" . auto-fill-mode)
+         ("C-c T t" . toggle-truncate-lines))
+  :init (add-hook 'org-mode-hook 'turn-on-auto-fill)
+  :diminish auto-fill-mode)
+
+(use-package which-key
+  :ensure t
+  :defer 10
+  :diminish which-key-mode
+  :config
+
+  ;; Replacements for how KEY is replaced when which-key displays
+  ;;   KEY → FUNCTION
+  ;; Eg: After "C-c", display "right → winner-redo" as "▶ → winner-redo"
+  (setq which-key-key-replacement-alist
+        '(("<\\([[:alnum:]-]+\\)>" . "\\1")
+          ("left"                  . "◀")
+          ("right"                 . "▶")
+          ("up"                    . "▲")
+          ("down"                  . "▼")
+          ("delete"                . "DEL") ; delete key
+          ("\\`DEL\\'"             . "BS") ; backspace key
+          ("next"                  . "PgDn")
+          ("prior"                 . "PgUp"))
+
+        ;; List of "special" keys for which a KEY is displayed as just
+        ;; K but with "inverted video" face... not sure I like this.
+        which-key-special-keys '("RET" "DEL" ; delete key
+                                 "ESC" "BS" ; backspace key
+                                 "SPC" "TAB")
+
+        ;; Replacements for how part or whole of FUNCTION is replaced:
+        which-key-description-replacement-alist
+        '(("Prefix Command" . "prefix")
+          ("\\`calc-"       . "") ; Hide "calc-" prefixes when listing M-x calc keys
+          ("\\`projectile-" . "𝓟/")
+          ("\\`org-babel-"  . "ob/"))
+
+        ;; Underlines commands to emphasize some functions:
+        which-key-highlighted-command-list
+        '("\\(rectangle-\\)\\|\\(-rectangle\\)"
+          "\\`org-"))
+
+  ;; Change what string to display for a given *complete* key binding
+  ;; Eg: After "C-x", display "8 → +unicode" instead of "8 → +prefix"
+  (which-key-add-key-based-replacements
+    "C-x 8"   "unicode"
+    "C-c T"   "toggles-"
+    "C-c p s" "projectile-search"
+    "C-c p 4" "projectile-other-buffer-"
+    "C-x a"   "abbrev/expand"
+    "C-x r"   "rect/reg"
+    "C-c /"   "engine-mode-map"
+    "C-c C-v" "org-babel")
+
+  (which-key-mode 1))
+
+(use-package fancy-narrow
+  :ensure t
+  :config
+  (defun ha/highlight-block ()
+    "Highlights a 'block' in a buffer defined by the first blank
+     line before and after the current cursor position. Uses the
+     'fancy-narrow' mode to high-light the block."
+    (interactive)
+    (let (cur beg end)
+      (setq cur (point))
+      (setq end (or (re-search-forward  "^\s*$" nil t) (point-max)))
+      (goto-char cur)
+      (setq beg (or (re-search-backward "^\s*$" nil t) (point-min)))
+      (fancy-narrow-to-region beg end)
+      (goto-char cur)))
+
+  (defun ha/highlight-section (num)
+    "If some of the buffer is highlighted with the `fancy-narrow'
+     mode, then un-highlight it by calling `fancy-widen'.
+
+     If region is active, call `fancy-narrow-to-region'.
+
+     If NUM is 0, highlight the current block (delimited by blank
+     lines). If NUM is positive or negative, highlight that number
+     of lines.  Otherwise, called `fancy-narrow-to-defun', to
+     highlight current function."
+    (interactive "p")
+    (cond
+     ((fancy-narrow-active-p)  (fancy-widen))
+     ((region-active-p)        (fancy-narrow-to-region (region-beginning) (region-end)))
+     ((= num 0)                (ha/highlight-block))
+     ((= num 1)                (fancy-narrow-to-defun))
+     (t                        (progn (ha/expand-region num)
+                                      (fancy-narrow-to-region (region-beginning) (region-end))
+                                      (setq mark-active nil)))))
+
+  :bind (("C-M-+" . ha/highlight-section)
+         ("C-<f12>" . ha/highlight-section)))
+
+
+(use-package fancy-narrow
+  :ensure t
+  :config
+  (defun ha/highlight-block ()
+    "Highlights a 'block' in a buffer defined by the first blank
+     line before and after the current cursor position. Uses the
+     'fancy-narrow' mode to high-light the block."
+    (interactive)
+    (let (cur beg end)
+      (setq cur (point))
+      (setq end (or (re-search-forward  "^\s*$" nil t) (point-max)))
+      (goto-char cur)
+      (setq beg (or (re-search-backward "^\s*$" nil t) (point-min)))
+      (fancy-narrow-to-region beg end)
+      (goto-char cur)))
+
+  (defun ha/highlight-section (num)
+    "If some of the buffer is highlighted with the `fancy-narrow'
+     mode, then un-highlight it by calling `fancy-widen'.
+
+     If region is active, call `fancy-narrow-to-region'.
+
+     If NUM is 0, highlight the current block (delimited by blank
+     lines). If NUM is positive or negative, highlight that number
+     of lines.  Otherwise, called `fancy-narrow-to-defun', to
+     highlight current function."
+    (interactive "p")
+    (cond
+     ((fancy-narrow-active-p)  (fancy-widen))
+     ((region-active-p)        (fancy-narrow-to-region (region-beginning) (region-end)))
+     ((= num 0)                (ha/highlight-block))
+     ((= num 1)                (fancy-narrow-to-defun))
+     (t                        (progn (ha/expand-region num)
+                                      (fancy-narrow-to-region (region-beginning) (region-end))
+                                      (setq mark-active nil)))))
+
+  :bind (("C-M-+" . ha/highlight-section)
+         ("C-<f12>" . ha/highlight-section)))
+
+
+(defun narrow-or-widen-dwim (p)
+  "If the buffer is narrowed, it widens.  Otherwise, it narrows intelligently.
+Intelligently means: region, subtree, or defun, whichever applies
+first.
+
+With prefix P, don't widen, just narrow even if buffer is already
+narrowed."
+  (interactive "P")
+  (declare (interactive-only))
+  (cond ((and (buffer-narrowed-p) (not p)) (widen))
+        ((region-active-p)
+         (narrow-to-region (region-beginning) (region-end)))
+        ((derived-mode-p 'org-mode) (org-narrow-to-subtree))
+        (t (narrow-to-defun))))
+
+(global-set-key (kbd "C-x n x") 'narrow-or-widen-dwim)
+
+(use-package ace-window
+  :ensure t
+  :init
+    (setq aw-keys '(?a ?s ?d ?f ?j ?k ?l ?o))
+    (global-set-key (kbd "C-x o") 'ace-window)
+    :diminish ace-window-mode)
+
+(use-package avy
+  :ensure t
+  :init (setq avy-background t))
+
+(global-set-key (kbd "s-h") 'avy-goto-char-timer)
+(global-set-key (kbd "s-j") 'avy-goto-char-timer)
+(global-set-key (kbd "s-H") 'avy-pop-mark)
+(global-set-key (kbd "s-J") 'avy-pop-mark)
+(global-set-key (kbd "A-h") 'avy-goto-char-timer)
+(global-set-key (kbd "A-j") 'avy-goto-char-timer)
+(global-set-key (kbd "A-H") 'avy-pop-mark)
+(global-set-key (kbd "A-J") 'avy-pop-mark)
+
+
+(defun unfill-paragraph ()
+  "Convert a multi-line paragraph into a single line of text."
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
+
+;; Handy key definition
+(define-key global-map "\M-Q" 'unfill-paragraph)
+
+(use-package projectile
+  :ensure t
+  :init (projectile-global-mode 0)
+  :bind (("C-c p s" . projectile-ag)
+         ("C-c p g" . projectile-grep)
+         ("C-c p R" . projectile-regenerate-tags)))
+
+(use-package perspective
+  :ensure t
+  :bind ("C-x x x" . persp-switch-last)
+  :init (persp-mode +1)
+
+  (use-package persp-projectile
+    :ensure t
+    :bind ("C-x x P" . projectile-persp-switch-project))
+
+  :config
+    (setq persp-interactive-completion-function #'ido-completing-read)
+    (persp-turn-off-modestring))
+
+(use-package ido
+  :ensure t
+  :init  (setq ido-enable-flex-matching t
+               ido-ignore-extensions t
+               ido-use-virtual-buffers t
+               ido-everywhere t)
+  :config
+  (ido-mode 1)
+  (ido-everywhere 1)
+  (add-to-list 'completion-ignored-extensions ".pyc"))
+
+
+(use-package flx-ido
+   :ensure t
+   :init (setq ido-enable-flex-matching t
+               ido-use-faces nil)
+   :config (flx-ido-mode 1))
+
+(use-package ido-vertical-mode
+  :ensure t
+  :init               ; I like up and down arrow keys:
+  (setq ido-vertical-define-keys 'C-n-C-p-up-and-down)
+  :config
+  (ido-vertical-mode 1))
+
+(defun ido-sort-mtime ()
+  "Reorder the IDO file list to sort from most recently modified."
+  (setq ido-temp-list
+        (sort ido-temp-list
+              (lambda (a b)
+                (ignore-errors
+                  (time-less-p
+                   (sixth (file-attributes (concat ido-current-directory b)))
+                   (sixth (file-attributes (concat ido-current-directory a))))))))
+  (ido-to-end  ;; move . files to end (again)
+   (delq nil (mapcar
+              (lambda (x) (and (char-equal (string-to-char x) ?.) x))
+              ido-temp-list))))
+
+(add-hook 'ido-make-file-list-hook 'ido-sort-mtime)
+(add-hook 'ido-make-dir-list-hook 'ido-sort-mtime)
+
+(defadvice ido-find-file (after find-file-sudo activate)
+  "Find file as root if necessary."
+  (unless (and buffer-file-name
+               (file-writable-p buffer-file-name))
+    (let* ((file-name (buffer-file-name))
+           (file-root (if (string-match "/ssh:\\([^:]+\\):\\(.*\\)" file-name)
+                          (concat "/ssh:"  (match-string 1 file-name)
+                                  "|sudo:" (match-string 1 file-name)
+                                  ":"      (match-string 2 file-name))
+                        (concat "/sudo:localhost:" file-name))))
+      (find-alternate-file file-root))))
+
+(use-package smex
+  :ensure t
+  :init (smex-initialize)
+  :bind ("M-x" . smex)
+  ("M-X" . smex-major-mode-commands))
+
+(use-package helm
+  :ensure t
+  :init
+  (use-package helm-config))   ;; Binds C-x c to the helm bidness.
+
+(use-package helm-swoop
+  :ensure t
+  :init
+  ;; If this value is t, split window inside the current window
+  (setq helm-swoop-split-with-multiple-windows t
+        ;; If you prefer fuzzy matching
+        helm-swoop-use-fuzzy-match t)
+  :bind
+  (("M-s s" . helm-swoop)  ;; overbind M-i ?
+   ("M-s S" . helm-multi-swoop)))
+
+(use-package recentf
+  :init
+  (setq recentf-max-menu-items 25
+        recentf-auto-cleanup 'never
+        recentf-keep '(file-remote-p file-readable-p))
+  (recentf-mode 1)
+  (let ((last-ido "~/.emacs.d/ido.last"))
+    (when (file-exists-p last-ido)
+      (delete-file last-ido)))
+
+  :bind ("C-c f r" . recentf-open-files))
+
+(use-package flyspell
+  :ensure t
+  :diminish flyspell-mode
+  :init
+  (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+
+  (dolist (hook '(text-mode-hook org-mode-hook))
+    (add-hook hook (lambda () (flyspell-mode 1))))
+
+  (dolist (hook '(change-log-mode-hook log-edit-mode-hook org-agenda-mode-hook))
+    (add-hook hook (lambda () (flyspell-mode -1))))
+
+  :config
+  (setq ispell-program-name "/usr/local/bin/aspell"
+        ispell-local-dictionary "en_US"
+        ispell-dictionary "american" ; better for aspell
+        ispell-extra-args '("--sug-mode=ultra" "--lang=en_US")
+        ispell-list-command "--list"
+        ispell-local-dictionary-alist '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "['‘’]"
+                                      t ; Many other characters
+                                      ("-d" "en_US") nil utf-8))))
+
+(use-package flyspell
+  :config
+  (defun flyspell-detect-ispell-args (&optional run-together)
+    "if RUN-TOGETHER is true, spell check the CamelCase words."
+    (let (args)
+      (setq args (list "--sug-mode=ultra" "--lang=en_US"))
+      (if run-together
+          (setq args (append args '("--run-together" "--run-together-limit=5" "--run-together-min=2"))))
+      args))
+
+  ;; ispell-cmd-args is useless, it's the list of *extra* arguments we will append to the ispell process when "ispell-word" is called.
+  ;; ispell-extra-args is the command arguments which will *always* be used when start ispell process
+  (setq-default ispell-extra-args (flyspell-detect-ispell-args t))
+
+  (defadvice ispell-word (around my-ispell-word activate)
+    (let ((old-ispell-extra-args ispell-extra-args))
+      (ispell-kill-ispell t)
+      (setq ispell-extra-args (flyspell-detect-ispell-args))
+      ad-do-it
+      (setq ispell-extra-args old-ispell-extra-args)
+      (ispell-kill-ispell t)))
+
+  (defadvice flyspell-auto-correct-word (around my-flyspell-auto-correct-word activate)
+    (let ((old-ispell-extra-args ispell-extra-args))
+      (ispell-kill-ispell t)
+      ;; use emacs original arguments
+      (setq ispell-extra-args (flyspell-detect-ispell-args))
+      ad-do-it
+      ;; restore our own ispell arguments
+      (setq ispell-extra-args old-ispell-extra-args)
+      (ispell-kill-ispell t)))
+
+  (defun text-mode-hook-setup ()
+    ;; Turn off RUN-TOGETHER option when spell check text-mode
+    (setq-local ispell-extra-args (flyspell-detect-ispell-args)))
+
+  (add-hook 'text-mode-hook 'text-mode-hook-setup))
+
+(use-package linum
+  :init
+  (add-hook 'prog-mode-hook 'linum-mode)
+  (add-hook 'linum-mode-hook (lambda () (set-face-attribute 'linum nil :height 110)))
+
+  :config
+  (defun linum-fringe-toggle ()
+    "Toggles the line numbers as well as the fringe."    (interactive)
+    (cond (linum-mode (fringe-mode '(0 . 0))
+                      (linum-mode -1))
+          (t          (fringe-mode '(8 . 0))
+                      (linum-mode 1))))
+
+  :bind (("A-C-k"   . linum-mode)
+         ("s-C-k"   . linum-mode)
+         ("A-C-M-k" . linum-fringe-toggle)
+         ("s-C-M-k" . linum-fringe-toggle)))
+
+(use-package linum-relative
+  :ensure t
+  :config
+  (defun linum-new-mode ()
+    "If line numbers aren't displayed, then display them.
+     Otherwise, toggle between absolute and relative numbers."
+    (interactive)
+    (if linum-mode
+        (linum-relative-toggle)
+      (linum-mode 1)))
+
+  :bind ("A-k" . linum-new-mode)
+  ("s-k" . linum-new-mode))   ;; For Linux
+
+(use-package smart-comment
+  :bind ("M-;" . smart-comment))
+
+(use-package smartscan
+  :ensure t
+  :bind ("M-n" . smartscan-symbol-go-forward)
+  ("M-p" . smartscan-symbol-go-backward))
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(use-package flycheck
+  :ensure t
+  :init
+  (add-hook 'after-init-hook 'global-flycheck-mode)
+  :config
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
+
+(use-package git-gutter-fringe
+   :ensure t
+   :diminish git-gutter-mode
+   :init (setq git-gutter-fr:side 'right-fringe)
+   :config (global-git-gutter-mode t))
+
+(use-package gitconfig-mode
+  :ensure t)
+
+(use-package gitignore-mode
+  :ensure t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (zenburn-theme gitignore-mode gitconfig-mode git-gutter-fringe flycheck smartscan linum-relative yasnippet helm-swoop helm smex ido-vertical-mode flx-ido workgroups persp-projectile perspective projectile which-key use-package fancy-narrow f ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(use-package zenburn-theme
+  :ensure t
+  :config
+  (load-theme 'zenburn t)
+  )
