dfd734a91e468fc4354d6667ea7b8fd09e3656fe
Wang, Gang <qichfan@gmail.com>
2019-09-14 18:22:02 -0700

    Update init.el

--- init.el
+++ init.el
@@ -530,7 +530,8 @@ narrowed."
 ;;;
 
 (use-package web-mode
-  :mode ("\\.html$" . web-mode)
+  :mode (("\\.html$" . web-mode)
+         ("\\.jsx?$" . web-mode)) ;; auto-enable for .js/.jsx files
   :init
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
