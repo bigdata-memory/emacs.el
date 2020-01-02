7f55fffb32534685326e1df71467033e86bb59b5
Wang, Gang <qichfan@gmail.com>
2020-01-02 15:26:56 -0800

    Cannot load highlight-indentation

--- init.el
+++ init.el
@@ -601,13 +601,13 @@ narrowed."
   (bind-key "{" #'paredit-open-curly json-mode-map)
   (bind-key "}" #'paredit-close-curly json-mode-map))
 
-(use-package highlight-indentation
-  :init
-  (progn
-    (defun set-hl-indent-color ()
-      (set-face-background 'highlight-indentation-face "#e3e3d3"))
-    (add-hook 'python-mode-hook 'highlight-indentation-mode)
-    (add-hook 'python-mode-hook 'set-hl-indent-color)))
+;(use-package highlight-indentation
+;  :init
+;  (progn
+;    (defun set-hl-indent-color ()
+;      (set-face-background 'highlight-indentation-face "#e3e3d3"))
+;    (add-hook 'python-mode-hook 'highlight-indentation-mode)
+;    (add-hook 'python-mode-hook 'set-hl-indent-color)))
 
 (use-package tuareg
    :ensure t
