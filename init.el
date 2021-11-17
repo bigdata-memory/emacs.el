904078ea20c9e3836f42f9648c8f6ecdbeeae6df
Gary <gordon.king@intel.com>
2021-11-16 23:29:04 -0800

    add org-roam package

--- init.el
+++ init.el
@@ -848,3 +848,21 @@ narrowed."
 )
 
 (add-to-list 'exec-path (concat (getenv "HOME") "/.ghcup/bin"))
+
+(setq org-directory (concat (getenv "HOME") "/Documents/org-roam/"))
+
+(use-package org-roam
+  :init (setq org-roam-v2-ack t) ;; Acknowledge V2 upgrade
+  :custom
+  (org-roam-directory (file-truename org-directory))
+  :config
+  (org-roam-setup)
+  :bind (("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n r" . org-roam-node-random)
+         (:map org-mode-map
+               (("C-c n i" . org-roam-node-insert)
+                ("C-c n o" . org-id-get-create)
+                ("C-c n t" . org-roam-tag-add)
+                ("C-c n a" . org-roam-alias-add)
+                ("C-c n l" . org-roam-buffer-toggle)))))
